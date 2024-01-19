e1bbe37ef8046b33a1b3fcf3a6b0e5e70ed13349
nicema0 <80997227+nicema0@users.noreply.github.com>
2024-01-19 16:05:50 +0900

    Add files via upload

--- a.js
+++ a.js
@@ -1,8 +1,46 @@
-function show01(){
-	var food = document.getElementsByName("food")[0];
-	// selectedIndex : 선택한 option의 index 반환
-	var idx = food.selectedIndex;
-	// options : select 태그가 가진 option 태그들
-	var foodName = food.options[idx].value;
-	alert(foodName + " 먹어야지먹어야지먹어야지먹어야지먹어야지먹어야지!");
+
+// Test data
+const objTest = {
+fruits: ['apple', 'banana', 'peach', 'mango', 'kiwi'],
+vehicles: ['Airplain', 'Scooter', 'Bus', 'Train', 'Van'],
+brands: ['Apple', 'Google', 'Microsoft', 'Amazon', 'Tesla'],
+footballPlayers: ['Pele', 'Maradona', 'Messi', 'Ronaldo', 'Ronaldinho']
+};
+
+$(function() {
+init();
+
+$(document).on('change', 'select[name=sbx_class]', function() {
+const classVal = $(this).val();
+$('select[name=sbx_item] option').each(function(idx, item) {
+if ($(this).data('class') == classVal || $(this).val() == '') {
+	$(this).show();
+} else {
+	$(this).hide();
+}
+});
+$('select[name=sbx_item]').val('');
+})
+});
+
+// Selectbox option 생성
+function init() {
+let sClassHtml = '<option value="">선택하세요.</option>';
+let sItemHtml = '<option value="">선택하세요.</option>';
+
+for (const key in objTest) {
+sClassHtml += `<option value="${key}">${key}</option>`;
+
+const list = objTest[key];
+for (let i = 0; i < list.length; i++) {
+sItemHtml += `<option value="${list[i]}" data-class="${key}">${list[i]}</option>`;
+}
+}
+$('select[name=sbx_class]').html(sClassHtml);
+$('select[name=sbx_item]').html(sItemHtml);
+
+$('select[name=sbx_item] option').each(function(idx, item) {
+if ($(this).val() == '') return true;
+$(this).hide();
+});
 }
